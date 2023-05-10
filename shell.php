8ed900d7a095523f7e8369a470655ccc05a44b55
Vincent Schneider <vincent.schneider@sudile.com>
2023-05-10 06:05:30 -0700

    updated pclose to proc_close and moved functions to the wanted positions

--- shell.php
+++ shell.php
@@ -8,6 +8,46 @@ function expandPath($path) {
     return $path;
 }
 
+function allFunctionExist($list = []) {
+    foreach ($list as $entry) {
+        if (!function_exists($entry)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function executeCommand($cmd) {
+    $output = '';
+    if (function_exists('exec')) {
+        exec($cmd, $output);
+        $output = implode("\n", $output);
+    } else if (function_exists('shell_exec')) {
+        $output = shell_exec($cmd);
+    } else if (allFunctionExist(['system', 'ob_start', 'ob_get_contents', 'ob_end_clean'])) {
+        ob_start();
+        system($cmd);
+        $output = ob_get_contents();
+        ob_end_clean();
+    } else if (allFunctionExist(['passthru', 'ob_start', 'ob_get_contents', 'ob_end_clean'])) {
+        ob_start();
+        passthru($cmd);
+        $output = ob_get_contents();
+        ob_end_clean();
+    } else if (allFunctionExist(['popen', 'feof', 'fread', 'pclose'])) {
+        $handle = popen($cmd, 'r');
+        while (!feof($handle)) {
+            $output .= fread($handle, 4096);
+        }
+        pclose($handle);
+    } else if (allFunctionExist(['proc_open', 'stream_get_contents', 'proc_close'])) {
+        $handle = proc_open($cmd, [0 => ['pipe', 'r'], 1 => ['pipe', 'w']], $pipes);
+        $output = stream_get_contents($pipes[1]);
+        proc_close($handle);
+    }
+    return $output;
+}
+
 function featureShell($cmd, $cwd) {
     $stdout = "";
 
@@ -30,46 +70,6 @@ function featureShell($cmd, $cwd) {
         "stdout" => $stdout,
         "cwd" => getcwd()
     );
-}
-
-function executeCommand($cmd) {
-    $output = "";
-    if (function_exists("exec")) {
-        exec($cmd, $output);
-        $output = implode("\n", $output);
-    } else if (function_exists("shell_exec")) {
-        $output = shell_exec($cmd);
-    } else if (allFunctionExist(["system", "ob_start", "ob_get_contents", "ob_end_clean"])) {
-        ob_start();
-        system($cmd);
-        $output = ob_get_contents();
-        ob_end_clean();
-    } else if (allFunctionExist(["passthru", "ob_start", "ob_get_contents", "ob_end_clean"])) {
-        ob_start();
-        passthru($cmd);
-        $output = ob_get_contents();
-        ob_end_clean();
-    } else if (allFunctionExist(["popen", "feof", "fread", "pclose"])) {
-        $handle = popen($cmd, 'r');
-        while (!feof($handle)) {
-            $output .= fread($handle, 4096);
-        }
-        pclose($handle);
-    } else if (allFunctionExist(["proc_open", "stream_get_contents", "pclose"])) {
-        $handle = proc_open($cmd, [0 => ["pipe", "r"], 1 => ["pipe", "w"]], $pipes);
-        $output = stream_get_contents($pipes[1]);
-        pclose($handle);
-    }
-    return $output;
-}
-
-function allFunctionExist($list = []) {
-    foreach ($list as $entry) {
-        if (!function_exists($entry)) {
-            return false;
-        }
-    }
-    return true;
 }
 
 function featurePwd() {
